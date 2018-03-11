5d277f9b70a1d654f1ec00d50392aeba4a142d6e
shalahuddinraj <smshalahuddinraj@gmail.com>
2018-03-11 14:02:59 +0600

    first commit

---
+++ rajpingponggame/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 4;
+        public int speed_top = 4;
+        public int points = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled=true;
+            Cursor.Hide();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            racket.Top = playground.Bottom - (playground.Bottom / 10);
+            gameover_lbl.Left = (playground.Width / 2) - (gameover_lbl.Height / 2);
+            gameover_lbl.Top = (playground.Width / 2) - (gameover_lbl.Height / 2);
+            gameover_lbl.Visible = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int A = rand.Next(0, 255);
+            int R = rand.Next(0, 255);
+            int G = rand.Next(0, 255);
+            int B = rand.Next(0, 255);
+            racket.ForeColor = Color.FromArgb(A, R, G, B);
+            racket.Location = new Point(racket.Location.X + 5, racket.Location.Y);
+            if (racket.Location.X > this.Width)
+            {
+
+                racket.Location = new Point(0 - racket.Width, racket.Location.Y);
+            }
+            racket.Left = Cursor.Position.X - (racket.Width / 2);
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+            if (ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom && ball.Left >= racket.Left && ball.Right <= racket.Right)
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top;
+                points += 1;
+                poient_lbl.Text = points.ToString();
+                Random r= new Random();
+                //playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+
+            }
+            if (ball.Left <= playground.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+           }
+            if (ball.Top <= playground.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if (ball.Bottom >= playground.Bottom)
+            {
+                timer1.Enabled = false;
+                gameover_lbl.Visible = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) { this.Close(); }
+            if (e.KeyCode == Keys.F1)
+            {
+                ball.Top = 50;
+                ball.Left = 50;
+                speed_left = 4;
+                speed_top = 4;
+                points = 0;
+                poient_lbl.Text = "0";
+                timer1.Enabled = true;
+                gameover_lbl.Visible = false;
+
+            
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
